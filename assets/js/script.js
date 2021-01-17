9a0d6353a2fa882f3ec449f210df11cdaebf20ee
celaeon <celaeon@gmail.com>
2021-01-17 01:34:32 -0500

    added forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search")
-var mainContainer = document.querySelector("#card");
 
 $('#searchBox').on('click', function() {
     value = document.getElementById("city").value;
     console.log(value);
-var obj;
+    var obj;
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=" + value + "&appid=ab27f177782f621190cdae30ab462244")
 .then(res => res.json())
@@ -14,9 +11,6 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + value + "&appid=ab2
 
   search(event);
 });
-
-var mainCard = $(".card-body");
-    
 
 function search(event) {
     event.preventDefault();
@@ -47,5 +41,31 @@ function formatSearch(jsonObject) {
     $("#city-temp").text("Temperature" + ":" + " " + city_temp + " " + "Celsius");
     $("#city-hum").text("Humidity" + ":" + " " + city_hum + "%");
     $("#city-wind").text("Wind Speed" + ":" + " " + city_wind + " " + "MPH");
-}
+
+
+// 5 day forcast 
+$.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city_name + "&appid=ab27f177782f621190cdae30ab462244", 
+    method: "GET"
+}).then(function (response) {
+    for (i = 0; i < 5; i++) {
+        var newCard = $("<div>").attr("class", "col fiveDay bg-primary text-white rounded-lg p-2");
+        $("#weeklyForecast").append(newCard);
+
+        var myDate = new Date(response.list[i * 8].dt * 1000);
+        newCard.append($("<h4>").html(myDate.toLocaleDateString()));
+
+        var iconCode = response.list[i * 8].weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        newCard.append($("<img>").attr("src", iconURL));
+
+        var temp = Math.round((response.list[i * 8].main.temp - 273.15) * 1.80 + 32);
+        newCard.append($("<p>").html("Temp: " + temp + " &#8457"));
+
+        var humidity = response.list[i * 8].main.humidity;
+        newCard.append($("<p>").html("Humidity: " + humidity)); 
+    }
+});
+
+};
 
